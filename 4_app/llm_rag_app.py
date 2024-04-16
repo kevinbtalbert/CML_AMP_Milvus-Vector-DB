b0606959de5cadc307acc63dd6ff3560ba34c243
Kevin Talbert <ktalbert@cloudera.com>
2024-04-16 10:11:38 -0400

    setup milvus vector db

---
+++ 4_app/llm_rag_app.py
@@ -0,0 +1,16 @@
+import os
+import gradio
+
+from milvus import default_server
+from pymilvus import connections, Collection
+import utils.model_llm_utils as model_llm
+import utils.vector_db_utils as vector_db
+import utils.model_embedding_utils as model_embedding
+
+
+def main():
+    # Configure gradio QA app 
+    print("Starting Milvus Server...")
+
+if __name__ == "__main__":
+    main()
